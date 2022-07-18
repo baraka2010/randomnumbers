69a0b345229e87b3aa5f230e7058b8469be25932
mtanzile <bahatibaraka2000@gmail.com>
2022-07-18 23:58:28 +0300

    RandomNumbers.java

---
+++ RandomNumbers.java
@@ -0,0 +1,9 @@
+import java.util.Math;
+
+public class RandomNumbers {
+    public  static void main(String args[]){
+        System.out,println("1st" + Math.random());
+        System.out,println("2nd" + Math.random());
+        System.out,println("3rd" + Math.random());
+    }
+}
